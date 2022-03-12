8cbdd741ed0f91fb4438d67c7dfd8bb0095ab7f4
Scott Long <longstoryscott@gmail.com>
2022-03-12 11:31:32 -0600

    solves boomerang

--- boomerang.py
+++ boomerang.py
@@ -1,6 +1,6 @@
+from itertools import combinations
 import math
-from typing import Sequence, Tuple
-
+from typing import List, Sequence, Set, Tuple
 
 # Imagine a sky full of stars, represented by a sequence of
 # two-dimensional coordinate points. Defining a "boomerang"
@@ -9,8 +9,46 @@ from typing import Sequence, Tuple
 # definition, implement the boomerang_count function, which
 # computes the count of unique boomerangs that may be
 # formed from a provided list of stars.
+
+
+def get_equidistant_stars(origin_star: Tuple[int, int], stars: Set[Tuple[int, int]]) -> List:
+    destinations = []
+    distances = []
+    unique_distances = {}
+    for star in stars:
+        a = abs(origin_star[0] - star[0])
+        b = abs(origin_star[1] - star[1])
+        c_squared = a**2 + b**2
+        # second test fails without round ...
+        # distance is 1.9999999999999998 from star 2 and 2.0 from star 1
+        distance = round(math.sqrt(c_squared), 2)
+        destinations.append(star)
+        distances.append(distance)
+
+        if distance != 0.0:
+            if distance not in unique_distances.keys():
+                unique_distances[distance] = 1
+            else:
+                unique_distances[distance] = unique_distances[distance] + 1
+
+    destination_distances = zip(destinations, distances)
+
+    # assigns number to star to get unique combinations later
+    boomerang = [i+1 for i, (dest, dist)
+                 in enumerate(destination_distances)
+                 if dist in unique_distances.keys() and unique_distances[dist] > 1]
+
+    return boomerang
+
+
 def boomerang_count(stars: Sequence[Tuple[int, int]]) -> int:
-    pass
+    boomerangs = 0
+    for star in stars:
+        boomerang = get_equidistant_stars(star, stars)
+        boomerang_combos = combinations(boomerang, 2)
+        boomerangs += sum(1 for _ in boomerang_combos)
+
+    return boomerangs
 
 
 # *********************************************************
@@ -23,31 +61,36 @@ def boomerang_count(stars: Sequence[Tuple[int, int]]) -> int:
 # Simple 3 stars in a row:
 # A--B--C.
 # Boomerangs: ABC
-assert boomerang_count((
+test_one = boomerang_count((
     (0, 0),
     (1, 0),
     (2, 0),
-)) == 1
-
+))
+assert test_one == 1
+print(test_one)
 # Equilateral triangle:
-# A
-# / \
+#   A
+#  / \
 # B---C
 # Boomerangs: ABC, ACB, CAB
-assert boomerang_count((
+test_two = boomerang_count((
     (0, 0),
     (1, math.sqrt(3)),
     (2, 0),
-)) == 3
+))
+assert test_two == 3
+print(test_two)
 
 # Hub and Spoke:
-# C
-# |
+#    C
+#    |
 # B--A--D
 # Boomerangs: BAC, CAD, BAD, BCD
-assert boomerang_count((
+test_three = boomerang_count((
     (0, 0),
     (-1, 0),
     (0, 1),
     (1, 0),
-)) == 4
+))
+assert test_three == 4
+print(test_three)
--- makefile
+++ makefile
@@ -1,3 +1,6 @@
 install:
 	npm install
 	pip3 install -r requirements.txt
+
+boomerang:
+	python3 boomerang.py
